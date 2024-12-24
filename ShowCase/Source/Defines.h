e0378abf0b2baa875ecd26b26e53bfda2939a226
Pavel <pzabelin70@gmail.com>
2024-12-24 08:42:01 +0200

    Namespace for defines

--- ShowCase/Source/Defines.h
+++ ShowCase/Source/Defines.h
@@ -1,8 +1,10 @@
 #pragma once
 
-constexpr int WINDOW_WIDTH = 1600;
-constexpr int WINDOW_HEIGHT = 900;
-constexpr float ASPECT_RATIO = static_cast<float>(WINDOW_WIDTH) / WINDOW_HEIGHT;
+namespace ShowcaseConfig 
+{
+	constexpr int WINDOW_WIDTH = 1600;
+	constexpr int WINDOW_HEIGHT = 900;
+	constexpr float ASPECT_RATIO = static_cast<float>(WINDOW_WIDTH) / WINDOW_HEIGHT;
 
-constexpr float GRAVITY = 9.8f;
-constexpr float MAX_FPS = 60.0f;
+	constexpr float MAX_FPS = 60.0f;
+}
